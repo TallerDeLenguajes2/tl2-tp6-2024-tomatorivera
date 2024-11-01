f2799d36f0f8361cda1de5edd871f04cd188c54d
asdasd <marianokpo7@gmail.com>
2024-11-01 09:29:43 -0300

    Creación del proyecto dotnet mvc y gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp6_2024_tomatorivera.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
