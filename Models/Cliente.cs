c636dd89b50e50a0d9487453cd750dd1cbed42a2
tomatorivera <tomatorivera@gmail.com>
2024-11-08 17:09:57 -0300

    Agregué la clase modelo Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,30 @@
+namespace Models;
+
+public class Cliente
+{
+    private int id;
+    private string nombre;
+    private string email;
+    private string telefono;
+
+    public Cliente()
+    {
+        id = -1;
+        nombre = string.Empty;
+        email = string.Empty;
+        telefono = string.Empty;
+    }
+
+    public Cliente(int id, string nombre, string email, string telefono)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public int Id { get => id; set => id = value; }
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Email { get => email; set => email = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+}
