d79d656717e3ee84d69763eacc8c1fd2cc2766df
tomatorivera <tomatorivera@gmail.com>
2024-11-15 09:54:42 -0300

    Creé la View Listar de Cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Persistence;
+
+namespace tl2_tp6_2024_tomatorivera.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ILogger<ClienteController> _logger;
+        private readonly IRepository<Cliente> _repositorioClientes;
+        
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+            _repositorioClientes = new ClienteRepositoryImpl();
+        }
+
+        [HttpGet]
+        public IActionResult Listar() {
+            return View(_repositorioClientes.Listar());
+        }
+    }
+}
