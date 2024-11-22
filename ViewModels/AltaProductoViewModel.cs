c399f6057149267c6e1a5c6af9b03f8a8ca97b1c
tomatorivera <tomatorivera@gmail.com>
2024-11-22 02:30:53 -0300

    Agregué las nuevas validaciones solicitadas para el Alta de producto en su viewmodel

--- ViewModels/AltaProductoViewModel.cs
+++ ViewModels/AltaProductoViewModel.cs
@@ -4,9 +4,7 @@ namespace tl2_tp6_2024_tomatorivera.ViewModels;
 
 public class AltaProductoViewModel
 {
-    [Required(ErrorMessage = "El campo descripción es requerido")]
     private string _descripcion;
-    [Required(ErrorMessage = "El campo precio es requerido")]
     private int _precio;
 
     public AltaProductoViewModel() 
@@ -15,6 +13,9 @@ public class AltaProductoViewModel
         _precio = 0;
     }
 
+    [StringLength(250, ErrorMessage = "La longitud de {0} debe ser de máximo {1} caracteres")]
     public string Descripcion { get => _descripcion; set => _descripcion = value; }
+    [Required(ErrorMessage = "El campo precio es requerido")]
+    [Range(0, 9999999, ErrorMessage = "El valor del precio debe ser positivo")]
     public int Precio { get => _precio; set => _precio = value; }
 }
